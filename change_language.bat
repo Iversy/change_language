7378d0cf0e6a51948c097a9fb8d867f11b882575
Iversy <iversy49@ya.ru>
2024-08-12 03:30:04 +0500

    delete bloat files

--- change_language.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-taskkill /F /IM pythonw.exe
-start /MIN pythonw.exe %USERPROFILE%\Documents\change_language\change_language.py
--- help.py
+++
@@ -1,9 +0,0 @@
-
-en = "qwertyuiop[]asdfghjkl;'zxcvbnm,.`QWERTYUIOP{}ASDFGHJKL:\"ZXCVBNM<>~"
-ru = "йцукенгшщзхъфывапролджэячсмитьбюёЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮЁ"
-a = dict()
-for i in range(len(en)):
-    a[en[i]] = ru[i]
-    a[ru[i]] = en[i]
-    
-print(a)
